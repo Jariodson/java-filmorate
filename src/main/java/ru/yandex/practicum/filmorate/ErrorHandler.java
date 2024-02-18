635dc6f3ca04325f601538c3d04b024a01a271c7
Jarodson <jarodson@yandex.ru>
2024-02-18 23:14:05 +0300

    feat: добавить обработку ошибки валидации

--- src/main/java/ru/yandex/practicum/filmorate/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/ErrorHandler.java
@@ -1,17 +1,23 @@
 package ru.yandex.practicum.filmorate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
+@Slf4j
 public class ErrorHandler {
     @ExceptionHandler
     public ResponseEntity<Map<String, String>> handleInvalidId(final IllegalArgumentException e) {
+        log.warn("Ошибка! {}", e.getMessage());
         return new ResponseEntity<>(
                 Map.of("error", e.getMessage()), HttpStatus.NOT_FOUND
         );
@@ -19,8 +25,20 @@ public class ErrorHandler {
 
     @ExceptionHandler
     public ResponseEntity<Map<String, String>> handleValidationException(final ValidationException e) {
+        log.warn("Ошибка! {}", e.getMessage());
         return new ResponseEntity<>(
                 Map.of("error", e.getMessage()), HttpStatus.BAD_REQUEST
         );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 }
