88bb7b4d28ad8a8697f056ee5bf34932db37986a
jarodson <jarodson@yandex.ru>
2024-02-18 12:31:21 +0300

    refactor: добавить логирование, изменить сортровку в методе getFavouriteFilms

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -8,8 +8,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.film.InMemoryFilmStorage;
 
-import java.rmi.server.RemoteRef;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
@@ -19,53 +17,74 @@ import java.util.stream.Collectors;
 @Slf4j
 public class FilmService {
     FilmStorage filmStorage = new InMemoryFilmStorage();
-    public Collection<Film> getFilms(){
+
+    public Collection<Film> getFilms() {
+        log.info("Получен запрос GET на получение списка всех фильмов");
+        log.info("Размер списка фильмов: {}", filmStorage.getAllFilms().size());
         return filmStorage.getAllFilms();
     }
-    public Film getFilmById(Long id){
-        for (Film film : filmStorage.getAllFilms()){
-            if (id.equals(film.getId())){
-                return film;
-            }
+
+    public Film getFilmById(Long id) {
+        log.info("Получен запрос GET на получение фильма по ID: {}", id);
+        if (filmStorage.getFilmById(id) != null) {
+            return filmStorage.getFilmById(id);
         }
         throw new IllegalArgumentException("Фильма с ID " + id + "не существует!");
     }
-    public Collection<Film> getFavouriteFilms(int count){
+
+    public Collection<Film> getFavouriteFilms(int count) {
         log.info("Получен запрос GET на получение самых популярных фильмов!");
-        Film[] favouriteFilms = new Film[count];
-        List<Film> films = (List<Film>) filmStorage.getAllFilms();
-        films.sort(Comparator.comparingInt(o -> o.getLikes().size()));
-        for (int i = 0; i < favouriteFilms.length; i++) {
-            favouriteFilms[i] = films.get(i);
-        }
+        List<Film> films = filmStorage.getAllFilms().stream()
+                .sorted(Comparator.comparing(o -> {
+                    if (o.getLikes() == null) {
+                        return null;
+                    } else {
+                        return o.getLikes().size();
+                    }
+                }, Comparator.nullsLast(Comparator.naturalOrder())))
+                .limit(count)
+                .collect(Collectors.toList());
         log.info("Вывод {} самых популярных фильмов. Id фильмов: {}", count,
-                Arrays.stream(favouriteFilms).map(Film::getId).collect(Collectors.toList()));
-        return List.of(favouriteFilms);
+                films.stream().map(Film::getId).collect(Collectors.toList()));
+        return films;
     }
 
-    public ResponseEntity<Film> addFilm(Film film){
+    public ResponseEntity<Film> addFilm(Film film) {
+        log.info("Получен запрос POST на добавление фильма в список");
         filmStorage.addNewFilm(film);
+        log.info("Фильм добавлен в список: {}. Размер списка: {}", film, filmStorage.getAllFilms().size());
         return new ResponseEntity<>(film, HttpStatus.CREATED);
     }
-    public ResponseEntity<Film> addLike(Long filmId, Long userId){
-        Film film = getFilmById(filmId);
-        log.info("Получен запрос POST на добавление лайка");
+
+    public ResponseEntity<Film> addLike(Long filmId, Long userId) {
+        Film film = filmStorage.getFilmById(filmId);
+        log.info("Получен запрос POST на добавление лайка. Id фильма: {}, Id пользователя: {}", filmId, userId);
         film.addLike(userId);
-        log.info("Лайк успешно поставлен! Id пользователя: {}", userId);
-        return new ResponseEntity<>(film, HttpStatus.CREATED);
-    }
-    public ResponseEntity<Film> updateFilm(Film film){
-        filmStorage.updateFilm(film);
-        return new ResponseEntity<>(film, HttpStatus.OK);
-    }
-    public ResponseEntity<Film> deleteFilm(Film film){
-        filmStorage.deleteFilm(film);
+        log.info("Лайк успешно поставлен! Id фильма: {} ,Id пользователя: {}", filmId, userId);
         return new ResponseEntity<>(film, HttpStatus.OK);
     }
 
-    public ResponseEntity<Film> removeLike(Long filmId, Long userId){
+    public ResponseEntity<Film> updateFilm(Film film) {
+        log.info("Получен запрос PUT на обновления фильма в списке");
+        filmStorage.updateFilm(film);
+        log.info("Обновленный фильм: {} добавлен в список. Размер списка: {}", film, filmStorage.getAllFilms().size());
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Film> deleteFilm(Film film) {
+        log.info("Получен запрос DELETE на удаление фильма");
+        filmStorage.deleteFilm(film);
+        log.info("Фильм удалён! {}", film);
+        return new ResponseEntity<>(film, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Film> removeLike(Long filmId, Long userId) {
         log.info("Получен запрос DELETE на удаление лайка");
-        Film film = getFilmById(filmId);
+        Film film = filmStorage.getFilmById(filmId);
+        if (film.getLikes() == null) {
+            log.warn("Ошибка! У фильма с ID: {} еще нет лайков!", filmId);
+            return new ResponseEntity<>(film, HttpStatus.NOT_FOUND);
+        }
         film.removeLike(userId);
         log.info("Лайк пользователя {} успешно удалён!", userId);
         return new ResponseEntity<>(film, HttpStatus.OK);
