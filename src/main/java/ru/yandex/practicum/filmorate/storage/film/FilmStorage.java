4219b2b6a373a96db6f03598f8f0291e9598b427
jarodson <jarodson@yandex.ru>
2024-02-18 12:31:42 +0300

    refactor: измнеить тип возвращаемого значения

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,7 +6,12 @@ import java.util.Collection;
 
 public interface FilmStorage {
     Collection<Film> getAllFilms();
+
+    Film getFilmById(Long id);
+
     void addNewFilm(Film film);
+
     void updateFilm(Film film);
+
     void deleteFilm(Film film);
 }
