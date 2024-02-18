24852945a9649f90ab168f77e126ba2b8b9cbcd9
jarodson <jarodson@yandex.ru>
2024-02-18 12:27:33 +0300

    style: code style

--- src/main/java/ru/yandex/practicum/filmorate/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/ErrorHandler.java
@@ -2,7 +2,6 @@ package ru.yandex.practicum.filmorate;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
@@ -12,14 +11,14 @@ import java.util.Map;
 @RestControllerAdvice
 public class ErrorHandler {
     @ExceptionHandler
-    public ResponseEntity<Map<String, String>> handleInvalidId(final IllegalArgumentException e){
+    public ResponseEntity<Map<String, String>> handleInvalidId(final IllegalArgumentException e) {
         return new ResponseEntity<>(
                 Map.of("error", e.getMessage()), HttpStatus.NOT_FOUND
         );
     }
 
     @ExceptionHandler
-    public ResponseEntity<Map<String, String>> handleValidationException(final ValidationException e){
+    public ResponseEntity<Map<String, String>> handleValidationException(final ValidationException e) {
         return new ResponseEntity<>(
                 Map.of("error", e.getMessage()), HttpStatus.BAD_REQUEST
         );
