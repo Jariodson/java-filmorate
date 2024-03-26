5662e912aa0c3c2cdd85d6ccd397c55b44560f58
jarodson <jarodson@yandex.ru>
2024-03-26 17:58:01 +0300

    добавить серисы

---
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/GenreServiceImpl.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Genre;
+import ru.yandex.practicum.filmorate.service.GenreService;
+import ru.yandex.practicum.filmorate.storage.dal.GenreDal;
+
+import java.util.Collection;
+
+@Service
+public class GenreServiceImpl implements GenreService {
+    @Autowired
+    private GenreDal genreDao;
+    @Override
+    public Collection<Genre> getGenres() {
+        return genreDao.getGenres();
+    }
+
+    @Override
+    public Genre getGenreById(Long id) {
+        return genreDao.getGenreById(id);
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/service/impl/MpaServiceImpl.java
@@ -0,0 +1,24 @@
+package ru.yandex.practicum.filmorate.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.model.Mpa;
+import ru.yandex.practicum.filmorate.service.MpaService;
+import ru.yandex.practicum.filmorate.storage.dal.MpaDal;
+
+import java.util.Collection;
+
+@Service
+public class MpaServiceImpl implements MpaService {
+    @Autowired
+    private MpaDal mpaDao;
+    @Override
+    public Collection<Mpa> getMpa() {
+        return mpaDao.getMpa();
+    }
+
+    @Override
+    public Mpa getMpaById(Long id) {
+        return mpaDao.getMpaById(id);
+    }
+}
