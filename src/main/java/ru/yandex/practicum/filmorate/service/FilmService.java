a4244e753805c4d0ecd68fd57664cb56f8dd16f0
AndreyVolokhatyy <ftrixagain@gmail.com>
2024-04-03 22:19:18 +0300

    fix: style (delete import)

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import ru.yandex.practicum.filmorate.model.Film;
 
 import java.util.Collection;
-import java.util.Set;
 
 public interface FilmService {
     Collection<Film> getFilms();
