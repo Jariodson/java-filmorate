198e94455533441c8ba5edc7fa257c0ff6889575
iRiveri <rivers-property@yandex.ru>
2024-04-10 07:22:51 +0300

    feat: Попытка сделать верный парсинг enamов при отправке.

--- src/main/java/ru/yandex/practicum/filmorate/model/EventType.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/EventType.java
@@ -1,5 +1,8 @@
 package ru.yandex.practicum.filmorate.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
 public enum EventType {
     LIKE,
     REVIEW,
