d11d37c12fbe0eab50bd841efee2aff2e99f4aba
swwwer82 <swwwer82@yandex.ru>
2024-04-10 19:35:14 +0300

    delete unused

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -4,13 +4,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.MpaService;
-
 import javax.validation.constraints.NotNull;
 import java.util.Collection;
-import java.util.List;
 
 @Slf4j
 @RestController
